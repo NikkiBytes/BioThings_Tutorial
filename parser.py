1dd09a72bdd4e8610576e041bb1198e9a8b0b58f
Nichollette Acosta <nicholletteacosta@gmail.com>
2021-09-08 17:11:21 -0400

    added py file and parser yield statement need to add data upload path

--- parser.py
+++ parser.py
@@ -1,14 +1,29 @@
+
 import os, pandas, csv, re
-import math
+import numpy as np
+from biothings.utils.dataload import dict_convert, dict_sweep
 
 from biothings import config
-from biothings.utils.dataload import dict_convert
 logging = config.logger
 
+process_key = lambda k: k.replace(" ","_").lower()
 
-def load_orthology(data_folder):
-    # the path points to the target data
-    infile = os.path.join(data_folder, "ORTHOLOGY-ALLIANCE_COMBINED.tsv")
-    assert os.path.exists(infile)
+def load_orthology(data, data_list):
 
+    gene1_ids=data['Gene1ID']
+    gene2_ids=data["Gene2ID"]
 
+    assert len(gene1_ids) == len(gene2_ids)
+
+    results = {}
+
+    for rec in data_list:
+        _id = rec['Gene1ID']
+        rec = dict_convert(rec,keyfn=process_key)
+        # remove NaN values, not indexable
+        rec = dict_sweep(rec,vals=[np.nan])
+        results.setdefault(_id,[]).append(rec)
+        #print(rec)
+    for _id,docs in results.items():
+        doc = {"_id": _id, "orthology_data" : docs}
+        yield doc
