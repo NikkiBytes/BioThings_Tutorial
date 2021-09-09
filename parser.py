82d3a8f4cf58b84d396ea5c46cfe8022a174b830
Nichollette Acosta <nicholletteacosta@gmail.com>
2021-09-09 13:43:46 -0400

    added update to parser

--- parser.py
+++ parser.py
@@ -8,22 +8,20 @@ logging = config.logger
 
 process_key = lambda k: k.replace(" ","_").lower()
 
-def load_orthology(data, data_list):
-
-    gene1_ids=data['Gene1ID']
-    gene2_ids=data["Gene2ID"]
-
-    assert len(gene1_ids) == len(gene2_ids)
-
+def load_orthology(data_folder):
+    infile = os.path.join(data_folder, "ORTHOLOGY-ALLIANCE_COMBINED.tsv")
+    assert os.path.exists(infile)
+    data=pd.read_csv(f_in, header=15, sep="\\t").to_dict(orient='records')
     results = {}
 
-    for rec in data_list:
+    for rec in data:
         _id = rec['Gene1ID']
         rec = dict_convert(rec,keyfn=process_key)
         # remove NaN values, not indexable
         rec = dict_sweep(rec,vals=[np.nan])
         results.setdefault(_id,[]).append(rec)
         #print(rec)
+
     for _id,docs in results.items():
         doc = {"_id": _id, "orthology_data" : docs}
         yield doc
